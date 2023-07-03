9413148a5f81c92a8dd849afbb1dbffefbfbd08f
Elena Kuznetsova <lozhevaea@gmail.com>
2023-07-03 19:29:55 +0500

    add string massiv

--- task/Program.cs
+++ task/Program.cs
@@ -5,3 +5,16 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+System.Console.WriteLine("Введите:  ");
+string? str = Console.ReadLine();
+int count = str!.Length;
+string[] arr = new string[count];
+
+for (int i = 0; i < count; i++)
+{
+    arr[i] = Convert.ToString(str![i]); 
+}
+
+System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
