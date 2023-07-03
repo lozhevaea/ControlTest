deb4b788de7e1b166f13e27c8de8640cff7dc2f6
Elena Kuznetsova <lozhevaea@gmail.com>
2023-07-03 20:09:44 +0500

    add cod

--- task/Program.cs
+++ task/Program.cs
@@ -7,8 +7,52 @@
 // [“Russia”, “Denmark”, “Kazan”] → []
 
 
-Console.Clear();
-System.Console.WriteLine("Введите слова, числа, фразы через запятую: ");
-string? input = Console.ReadLine();
-string[] str = input!.Split(", ");
-System.Console.WriteLine("[ " + string.Join(", ", str) + " ]");
+void Main()
+{
+    Console.Clear();
+    System.Console.WriteLine("Введите слова, числа, фразы через запятую: ");
+    string? input = Console.ReadLine();
+    string[] str = input!.Split(", ");
+    System.Console.WriteLine("[ " + string.Join(", ", str) + " ]");
+    string[] arr = new string[NewArr(str)];
+    PrintArray(SecondArray(str, arr));
+}
+
+int NewArr(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] SecondArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+    return arr2;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($" {array[i]} ");
+    }
+    System.Console.Write("]");
+}
+
+Main();
